89c13a321b470ef29ed5c996133b0bcec0a466f2
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-12-10 13:56:10 -0800

    add summation.js

---
+++ CodeWars/Level 8/summation.js
@@ -0,0 +1,11 @@
+// http://www.codewars.com/kata/grasshopper-summation/train/javascript
+
+var summation = function (num) {
+  let sum = 0;
+
+  for (let i = 1; i <= num; i++){
+    sum += i;
+  }
+
+  return sum;
+}
