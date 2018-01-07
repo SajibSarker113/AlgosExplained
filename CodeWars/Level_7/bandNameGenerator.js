5375f0439b4afdd6b351038914f45241b557cca4
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-01-07 14:11:18 -0800

    add js file

---
+++ CodeWars/Level_7/bandNameGenerator.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/band-name-generator/train/javascript
+
+function bandNameGenerator(str) {
+  if (str[0] === str[str.length - 1]){
+    return double(str);
+  } else {
+    return addThe(str);
+  }
+}
+
+let addThe = (noun) => {
+  return "The " + noun.slice(0,1).toUpperCase() + noun.slice(1);
+}
+
+let double = (noun) => {
+  return noun.slice(0,1).toUpperCase() + noun.slice(1) + noun.slice(1);
+}
