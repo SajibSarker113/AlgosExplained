d14cf325158410da570137469a9b2094bdc2e991
Sung Kwang David Kim <sungkwangkim@gmail.com>
2018-01-07 14:32:04 -0800

    add js file

---
+++ CodeWars/Level_6/createPhoneNumber.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/create-phone-number/train/javascript
+
+function createPhoneNumber(numbers){
+  return "(" + numbers[0] + numbers[1] + numbers[2] + ") " +
+         numbers[3] + numbers[4] + numbers[5] + "-" +
+         numbers[6] + numbers[7] + numbers[8] + numbers[9];
+}
